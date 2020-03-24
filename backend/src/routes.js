2ebea0fe0994dc121be5cfa1cb22e70c1cfd6004
lucas beat <clashbruno10@gmail.com>
2020-03-24 12:43:13 -0300

    initial commit

---
+++ backend/src/routes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+
+const routes = express.Router();
+
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.store);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.store);
+routes.delete('/incidents/:id', IncidentController.destroy);
+
+module.exports = routes;
